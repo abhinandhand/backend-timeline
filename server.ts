a2a676653044cc8e7c78e9ed8000a13ab0124ee3
Abhinandhan Devadiga <psdabhi10@gmail.com>
2025-04-14 01:36:16 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -5,30 +5,6 @@ import cors from "cors";
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:4200",
-  "http://127.0.0.1:8081",
-  "https://nex-bank.pages.dev/",
-  process.env.RENDER_EXTERNAL_URL || "https://your-app.onrender.com", // Add Render URL
-];
-
-app.use(
-  cors({
-    origin: function (
-      origin: string | undefined,
-      callback: (err: Error | null, allow?: boolean) => void
-    ) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
-
 app.use(bodyParser.json()); // Support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ extended: false })); // Support URL-encoded bodies
 
